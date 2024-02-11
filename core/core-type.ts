2b49017bd505dfd7493edeb8b985e73737287e2e
Mukul Dutt <mukuldutt1234@gmail.com>
2024-02-11 22:53:49 +0100

    lib files refactored

--- core/core-type.ts
+++
@@ -1,6 +0,0 @@
-import { Schema } from "../schema";
-import { DataType, Type } from "../schema/type-core";
-
-export type JsonValidatorProps = {
-  schema: Type<DataType.OBJECT | DataType.ARRAY>;
-};
